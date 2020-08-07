6e9a587053ab5e192ddf56697e0501b07741b642
zoomi-raja <zoomi.raja@gmail.com>
2020-08-07 20:18:34 +0400

    main section header

---
+++ blog_frontend/src/components/home/header/Header.js
@@ -0,0 +1,110 @@
+import React from "react";
+import Typewriter from "typewriter-effect";
+import classes from "./Header.module.scss";
+//todo on click contact info should copy date to clipboard
+import pic from "../../../assets/zoomi.jpeg";
+const Header = () => {
+	return (
+		<div className="sub-container">
+			<div className={classes.header}>
+				<div className={classes.imgHolder}>
+					<img src={pic} alt="" />
+				</div>
+				<div className={classes.infoHolder}>
+					<div className={classes.infoPersonal}>
+						<p>
+							Hi,
+							<br /> I Wellcome you on my Portfolio
+							<br /> My name is Zamurd Ali
+						</p>
+						<span>
+							I am &nbsp;
+							<Typewriter
+								options={{
+									strings: [
+										"Learner ..!",
+										"Web Developer ..!",
+										"FP Gamer ..!",
+										"Programmer ..!",
+									],
+									changeDeleteSpeed: 0.5,
+									autoStart: true,
+									loop: true,
+								}}
+							/>
+						</span>
+					</div>
+					<div className={classes.contact}>
+						<div className={classes.contact_item}>
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								width="24"
+								height="24"
+								viewBox="0 0 24 24"
+							>
+								<path d="M17.5 2c.276 0 .5.224.5.5v19c0 .276-.224.5-.5.5h-11c-.276 0-.5-.224-.5-.5v-19c0-.276.224-.5.5-.5h11zm2.5 0c0-1.104-.896-2-2-2h-12c-1.104 0-2 .896-2 2v20c0 1.104.896 2 2 2h12c1.104 0 2-.896 2-2v-20zm-9.5 1h3c.276 0 .5.224.5.501 0 .275-.224.499-.5.499h-3c-.275 0-.5-.224-.5-.499 0-.277.225-.501.5-.501zm1.5 18c-.553 0-1-.448-1-1s.447-1 1-1c.552 0 .999.448.999 1s-.447 1-.999 1zm5-3h-10v-13h10v13z" />
+							</svg>
+							<span>+971-551299472</span>
+						</div>
+						<div className={classes.contact_item}>
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								width="24"
+								height="24"
+								viewBox="0 0 24 24"
+							>
+								<path d="M0 3v18h24v-18h-24zm21.518 2l-9.518 7.713-9.518-7.713h19.036zm-19.518 14v-11.817l10 8.104 10-8.104v11.817h-20z" />
+							</svg>
+							<span>zoomi.raja@gmail.com</span>
+						</div>
+					</div>
+					<div className={classes.socialIcons}>
+						<a
+							href="https://www.linkedin.com/in/zamurd-ali-b2232870/"
+							target="_blank"
+						>
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								width="24"
+								height="24"
+								viewBox="0 0 24 24"
+							>
+								<path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-2 16h-2v-6h2v6zm-1-6.891c-.607 0-1.1-.496-1.1-1.109 0-.612.492-1.109 1.1-1.109s1.1.497 1.1 1.109c0 .613-.493 1.109-1.1 1.109zm8 6.891h-1.998v-2.861c0-1.881-2.002-1.722-2.002 0v2.861h-2v-6h2v1.093c.872-1.616 4-1.736 4 1.548v3.359z" />
+							</svg>
+						</a>
+						<a href="https://github.com/zoomi-raja" target="_blank">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								width="24"
+								height="24"
+								viewBox="0 0 24 24"
+							>
+								<path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
+							</svg>
+						</a>
+						<a href="https://www.facebook.com/zoomi.raja/" target="_blank">
+							<svg
+								role="img"
+								viewBox="0 0 24 24"
+								xmlns="http://www.w3.org/2000/svg"
+							>
+								<path d="M23.9981 11.9991C23.9981 5.37216 18.626 0 11.9991 0C5.37216 0 0 5.37216 0 11.9991C0 17.9882 4.38789 22.9522 10.1242 23.8524V15.4676H7.07758V11.9991H10.1242V9.35553C10.1242 6.34826 11.9156 4.68714 14.6564 4.68714C15.9692 4.68714 17.3424 4.92149 17.3424 4.92149V7.87439H15.8294C14.3388 7.87439 13.8739 8.79933 13.8739 9.74824V11.9991H17.2018L16.6698 15.4676H13.8739V23.8524C19.6103 22.9522 23.9981 17.9882 23.9981 11.9991Z" />
+							</svg>
+						</a>
+						<a href="https://twitter.com/zoomiRajpoot" target="_blank">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								width="24"
+								height="24"
+								viewBox="0 0 24 24"
+							>
+								<path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6.066 9.645c.183 4.04-2.83 8.544-8.164 8.544-1.622 0-3.131-.476-4.402-1.291 1.524.18 3.045-.244 4.252-1.189-1.256-.023-2.317-.854-2.684-1.995.451.086.895.061 1.298-.049-1.381-.278-2.335-1.522-2.304-2.853.388.215.83.344 1.301.359-1.279-.855-1.641-2.544-.889-3.835 1.416 1.738 3.533 2.881 5.92 3.001-.419-1.796.944-3.527 2.799-3.527.825 0 1.572.349 2.096.907.654-.128 1.27-.368 1.824-.697-.215.671-.67 1.233-1.263 1.589.581-.07 1.135-.224 1.649-.453-.384.578-.87 1.084-1.433 1.489z" />
+							</svg>
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+export default Header;
--- blog_frontend/src/containers/home/Home.js
+++ blog_frontend/src/containers/home/Home.js
@@ -1,7 +1,18 @@
 import React from "react";
+import "./Home.scss";
+import Header from "../../components/home/header/Header";
 
 const Home = () => {
-	return <div>main playground</div>;
+	return (
+		<section className="section">
+			<Header />
+			<div className="sub-container">intro</div>
+			<div className="sub-container">intrests</div>
+			<div className="sub-container">motivation</div>
+			<div className="sub-container">portfolio</div>
+			<div className="sub-container">articles</div>
+		</section>
+	);
 };
 
 export default Home;
