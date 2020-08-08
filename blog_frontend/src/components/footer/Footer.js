6e63681e32f21190c5dfd47cd78b01268304fb6c
zoomi-raja <zoomi.raja@gmail.com>
2020-08-08 13:19:57 +0400

    footer basic done

--- blog_frontend/src/components/footer/Footer.js
+++ blog_frontend/src/components/footer/Footer.js
@@ -1,7 +1,14 @@
 import React from "react";
+import classes from "./Footer.module.scss";
+import Social from "../social/Social";
 
 const Footer = () => {
-	return <div>this is footer</div>;
+	return (
+		<div className={`sub-container ${classes.footer}`}>
+			<span> Built by zoomi, &copy; 2020, All Rights Reserved</span>
+			<Social />
+		</div>
+	);
 };
 
 export default Footer;
