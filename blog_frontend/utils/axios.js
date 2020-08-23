21daf41b1a3585e30b7a463fc39ea56b94af70ca
zoomi-raja <zoomi.raja@gmail.com>
2020-08-23 21:24:29 +0400

    aws test 9

--- blog_frontend/utils/axios.js
+++ blog_frontend/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const instance = axios.create({
-	baseURL: "http://localhost/",
+	baseURL: "http://localhost:8010/",
 	headers: { "Content-Type": "application/json" },
 });
 instance.interceptors.response.use(
