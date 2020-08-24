99576439d88ec76e1d550627ee6a8939625f7bee
zoomi-raja <zoomi.raja@gmail.com>
2020-08-24 23:07:12 +0400

    meta testing

--- blog_frontend/pages/index.js
+++ blog_frontend/pages/index.js
@@ -8,6 +8,11 @@ export default function Home() {
 			<Head>
 				<link rel="icon" href="/favicon.ico" />
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<meta name="author" content="Zamurd Ali"></meta>
+				<meta
+					name="description"
+					content="Portfolio website. Blog will be fully functional soon as 2020 is all about learning, struggle and moving forward but not to give up..!"
+				></meta>
 				<link
 					href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900"
 					rel="stylesheet"
