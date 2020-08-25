e46130871efb54ae728e586c7bc01d28363d9779
zoomi-raja <zoomi.raja@gmail.com>
2020-08-25 10:44:20 +0400

    paragraph change for intro

--- blog_frontend/portfolio/components/home/intro/Intro.js
+++ blog_frontend/portfolio/components/home/intro/Intro.js
@@ -7,18 +7,19 @@ const Intro = () => {
 				<span className="text-ul">Let me introduce my self</span>
 			</h1>
 			<p className="paragraph">
-				I am a university graduate and for me as a Web Developer it's been a 7
-				year long journey. My first programming language was C and throughout my
-				career i have learned various programming languages( JavaScript, Golang,
-				PHP ) and open source tools ( Git, Docker ). Started myprofessional
-				career by the start of 2013 as PHP, my main weapon along with it learnet
-				JavaScript by then JQuery was mainly used to make web pages
-				interactive.In Dec 2014 I got opportunity in one of the startup company
-				(awok.com) in Dubai, UAE. Worked for them until feb 2020 and through
-				this tenure learned Node and React. After getting free from a full time
-				job, I thought of going back to statically-typed type programming
-				language and came across golang. So now i would like to maintain arsenal
-				of JavaScript and Golang.
+				I am a University graduate and developing web systems in the last 7
+				years. My first programming language was C and throughout my career I
+				have learned various programming languages including but not limited to
+				JavaScript, GoLang, and PHP. I’ve worked on some open source tools as
+				Git and Docker. I started my professional career at the beginning of
+				2013 as PHP developer. My main weapon was PHP, along with JavaScript. At
+				the time jQuery was mostly used to develop interactive web pages. I
+				moved to Dubai in December 2014, with an opportunity in hands with a
+				startup (Awok.com), during my tenure till February 2020 I’ve learned
+				Node and React. As I’ve never stopped learning, during recent pandemic
+				after getting free from full time job I planned to learn
+				statically-typed programming language and came across GoLang, and now I
+				am maintaining arsenal of JavaScript and Golang.
 			</p>
 		</div>
 	);
