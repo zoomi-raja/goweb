2a83c3fd8ed8fe0e74abc2686e591c9a17d26d4a
zoomi-raja <zoomi.raja@gmail.com>
2020-11-06 00:18:58 +0500

    Updates

---
+++ precache-manifest.95f99db3056b8f1ba5b918cf3c2f1e7b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3b4fe0d31f8a9c5315c83e9aa0e6575",
+    "url": "/goweb/index.html"
+  },
+  {
+    "revision": "b2b909ef3962e3dfd08d",
+    "url": "/goweb/static/css/main.d0b6340a.chunk.css"
+  },
+  {
+    "revision": "f08474b5ab15fe418807",
+    "url": "/goweb/static/js/2.ddeac61f.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/goweb/static/js/2.ddeac61f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2b909ef3962e3dfd08d",
+    "url": "/goweb/static/js/main.0b5f645f.chunk.js"
+  },
+  {
+    "revision": "86945a89e90bb17c3ffd",
+    "url": "/goweb/static/js/runtime-main.fb3be651.js"
+  },
+  {
+    "revision": "8fdc2f9657ffd87e9b269e8248c16aed",
+    "url": "/goweb/static/media/golang.8fdc2f96.png"
+  },
+  {
+    "revision": "9282fd313c38fe08b4bc9809288328a2",
+    "url": "/goweb/static/media/zoomi.9282fd31.jpeg"
+  }
+]);
